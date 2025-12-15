af09bb979315f7fd05d27bf2a6ba352c4fc507e9
ykominami <ykominami@gmail.com>
2025-12-15 22:30:45 +0900

    add src_dir

---
+++ src/htmlparser/__init__.py
@@ -0,0 +1,18 @@
+# from yklibpy import yklibpy
+from yklibpy.htmlparser import {App, TopConfig, Util}
+from pathlib import Path
+from .abc import amain, bmain
+
+__all__ = [
+    "amain",
+    "bmain",
+]
+
+def cmain():
+    print("htmlparser cmain")
+
+
+
+if __name__ == "__main__"
+    amain()
+
